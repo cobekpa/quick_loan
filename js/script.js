5b880bc7a99205c699c97cdf7ddfa61fa363eb01
cobekpa <obekpacharles@yahoo.com>
2022-12-16 17:33:46 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*ScrollTop*/
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if(pos > 100) {
+        scrollProgress.style.display = "grid";
+    }else{
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient (#03cc65 ${scrollValue}%, #1ac008 ${scrollValue}% )`;
+};
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+
+/*Sticky Navbar*/
